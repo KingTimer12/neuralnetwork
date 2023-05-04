555e41bd38ec7f4cd2f3e50f072f3468fa099a47
KingTimer12 <queentimer12@gmail.com>
2023-05-04 14:57:54 -0300

    Atualização de arquivos e rede neural para machine learning simples

---
+++ simple-neural.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+def sigmoid(x):
+    return 1/(1+np.exp(-x))
+
+class Neuron:
+    def __init__(self, weights, bias):
+        self.weights = weights
+        self.bias = bias
+    
+    def feedforward(self, inputs):
+        total = np.dot(self.weights, inputs) + self.bias
+        return sigmoid(total)
+
+class NeuralNetwork:
+    def __init__(self):
+        weights = np.array([0, 1])
+        bias = 0
+
+        self.h1 = Neuron(weights, bias)
+        self.h2 = Neuron(weights, bias)
+        self.o1 = Neuron(weights, bias)
+
+    def feedforward(self, x):
+        out_h1 = self.h1.feedforward(x)
+        out_h2 = self.h2.feedforward(x)
+
+        out_o1 = self.o1.feedforward(np.array([out_h1, out_h2]))
+
+        return out_o1
+  
+network = NeuralNetwork()
+x = np.array([2, 3])
+print(network.feedforward(x)) # 0.7216325609518421
